f52485fdd9f7d5c9b82c8c8fd23772fdb4d2727f
kirabo <molutos@gmail.com>
2021-05-19 11:04:25 +0300

    added the responsive design. but a little more touch

--- js/cript.js
+++ js/cript.js
@@ -41,3 +41,24 @@ function  showSlide() {
     setTimeout(showSlide, 8000);
 }
 
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    //toggle Nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) =>{
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 1}s`
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
+} 
+
+navSlide();
